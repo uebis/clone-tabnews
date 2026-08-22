bfb990e76f4d53287325f3460adb3d15c33a2f4a
Andrezza Albuquerque <qa.andrezza+github@gmail.com>
2026-08-22 17:53:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Sinto sua falta 😅</h1>
+  return <h1>Sinto sua falta 😅</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
